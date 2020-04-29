2a045e63b7e8867da0906b0e86f3065b93806351
Laszlo Turanyi <turanyilaszlo95@gmail.com>
2020-04-29 21:27:46 +0200

    Modify build setup and force to use msvc compiler

--- setup.py
+++ setup.py
@@ -1,20 +1,27 @@
-import os, sys
+#import os, sys
 
-from distutils.core import setup, Extension
-from distutils import sysconfig
+#from distutils.core import setup, Extension
+#from distutils import sysconfig
 
-#cpp_args = ['-std=c++11', '-stdlib=libc++', '-mmacosx-version-min=10.7']
-
+from setuptools import setup, Extension
+from setuptools.command.build_ext import build_ext
+import sys
+import setuptools
 matext_module = Extension(
     'matext', sources = ['src/matrix.cpp', 'src/vector.cpp', 'src/main.cpp'],
     include_dirs=['pybind11/include', 'include'],
     language='c++',
-    #extra_compile_args = cpp_args,
     )
 
 setup(
     name = 'matext',
-    version = '1.0',
-    description = 'Python package with vector and matrix calculation C++ extension (PyBind11)',
+    version = '0.1',
+    description = 'C++ Extension for Python implementing Vector and Matrix classes',
+    url='https://github.com/laszloturanyi/matext',
+    author='Laszlo Turanyi',
+    author_email='turanyilaszlo95@gmail.com',
+    license='MIT',
+    setup_requires=['pybind11>=2.2'], 
+    install_requires=['pybind11>=2.2'],
     ext_modules = [matext_module],
 )
